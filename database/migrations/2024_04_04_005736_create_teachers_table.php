43413a74bc53c1e64089badae9c8289b0143ee20
Rafaeldm22 <rafael.moraes97@etec.sp.gov.br>
2024-04-04 20:08:41 -0300

    repositorio para php

---
+++ database/migrations/2024_04_04_005736_create_teachers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->id();
+            $$table-> string('name');
+            $$table-> char('cpf',lenght: 11);
+            $$table-> char('rg', lenght:9);
+            $$table-> string('NumberIdentification');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teachers');
+    }
+};
