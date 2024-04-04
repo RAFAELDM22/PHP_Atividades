336854b5fd4e7cdd01917e660979cfe5efd07413
RAFAEL MORAES <rafael.moraes97@etec.sp.gov.br>
2024-04-03 22:37:01 -0300

    subiu git

---
+++ migration-pratica/database/migrations/2024_03_28_012702_create_student_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->id();
+            $table-> string('name');
+            $table-> char('rm',lenght: 9);
+            $table-> char('RG',lenght: 9);
+            $table-> char('CPF', lenght: 11);
+            $table-> string('age');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student');
+    }
+};
